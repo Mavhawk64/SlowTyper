b6adeeb55c778160979fbb6f5539cdf9cd3d1a04
Maverick <38192173+Mavhawk64@users.noreply.github.com>
2021-12-16 17:53:26 -0600

    Add files via upload

---
+++ SlowRunner.java
@@ -0,0 +1,6 @@
+public class SlowRunner
+{
+    public static void main(String[] args) {
+        SlowTyper.print(500, "Hello World!");
+    }
+}
---
+++ SlowTyper.java
@@ -0,0 +1,15 @@
+public abstract class SlowTyper
+{
+    public static void print(int milliseconds, String word) {
+        char[] x = word.toCharArray();
+        for(int i = 0; i < x.length; i++) {
+            try {
+                Thread.sleep(milliseconds);
+                System.out.print(x[i]);
+            } catch(Exception e) {
+                System.err.println("Error in printSlowly(): " + e.toString());
+                break;
+            }
+        }
+    }
+}
